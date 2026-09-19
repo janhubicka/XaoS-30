03cc520a3ac633bffb10f836ddcfcfe3059a5a69
Jan Hubička <46065755+janhubicka@users.noreply.github.com>
2026-09-19 22:11:16 +0200

    Allow inline precision orbit state values

--- include/xaos/kernel.hpp
+++ include/xaos/kernel.hpp
@@ -52,17 +52,17 @@ using BurningShip=FormulaTag<Formula::BurningShip>;
 template<class Real,unsigned Scalars> struct OrbitScalars;
 template<class Real> struct OrbitScalars<Real,2> {
     Real x,y;
-    OrbitScalars() requires std::is_same_v<Real,double> = default;
+    OrbitScalars() requires (!std::is_same_v<Real,Big>) = default;
     explicit OrbitScalars(mp_bitcnt_t bits) requires std::is_same_v<Real,Big>:x(bits),y(bits) {}
 };
 template<class Real> struct OrbitScalars<Real,3> {
     Real x,y,a;
-    OrbitScalars() requires std::is_same_v<Real,double> = default;
+    OrbitScalars() requires (!std::is_same_v<Real,Big>) = default;
     explicit OrbitScalars(mp_bitcnt_t bits) requires std::is_same_v<Real,Big>:x(bits),y(bits),a(bits) {}
 };
 template<class Real> struct OrbitScalars<Real,4> {
     Real x,y,a,b;
-    OrbitScalars() requires std::is_same_v<Real,double> = default;
+    OrbitScalars() requires (!std::is_same_v<Real,Big>) = default;
     explicit OrbitScalars(mp_bitcnt_t bits) requires std::is_same_v<Real,Big>:x(bits),y(bits),a(bits),b(bits) {}
 };
 template<class Real,class F>
